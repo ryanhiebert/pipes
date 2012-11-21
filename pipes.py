67c51cd42e39b6b58d6aa57d95ca32b2b61dd351
Ryan Hiebert <ryan@ryanhiebert.com>
2012-11-21 13:45:20 -0800

    try my hand at fleshing it out

--- pipes.py
+++ pipes.py
@@ -1,6 +1,11 @@
-
 def pipe(*args):
     """Returns a pipe-enabled function"""
+
+    def pipemethod(arg, kwarg):
+        def ret(self):
+            return (arg, kwarg)
+        return ret
+
     if len(args) > 2:
         raise ValueError('Too many arguments')
     elif len(args) == 2:
@@ -9,10 +14,18 @@ def pipe(*args):
             raise ValueError('First Argument was not an int')
         if args[1] != None and not isinstance(basestring, args[1]):
             raise ValueError('Second Argument was not a str')
+        def wrapper(fun):
+            fun.__pipe__ = pipemethod(args[0], args[1])
+        return wrapper
     elif len(args) == 1:
         if isinstance(int, args[0]):
-            ### USE AS INT
+            def wrapper(fun):
+                fun__pipe__ = pipemethod(args[0], None)
+            return wrapper
         if isinstance(basestring, args[0]):
-            ### USE AS STR
+            def wrapper(fun):
+                fun.__pipe__ = pipemethod(None, args[0])
+            return wrapper
         else:
-            ### WRAP THE FUNCTION WITH DEFAULT 0
+            args[0].__pipe__ = pipemethod(0, None)
+            return args[0]
